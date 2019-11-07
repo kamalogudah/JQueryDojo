7c4dfbb175be9343b35aee0c0169c5bad3ee45e8
kamalogudah <mcpaul2058@gmail.com>
2019-11-07 10:18:54 +0300

    Select By Class

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
   //console.log("We are ready");
   // $('body').text('Hello World');
   // $('body').html('<strong>Hello World</strong>');
-  $('#fire').addClass('highlight');
+  //$('#fire').addClass('highlight'); //selecting by id
+  // $('.non-solid').addClass('highlight');
+  $('#container >>> .non-solid').addClass('highlight');
 });
