18a240f210944a774c694ea57c51a550f337c8e6
kamalogudah <mcpaul2058@gmail.com>
2019-11-10 01:05:05 +0300

    Change Events

--- js/main.js
+++ js/main.js
@@ -8,9 +8,20 @@ $(document).ready(function(){
   $('#container >>> .non-solid').addClass('highlight');
   $('#container input:required').addClass('highlight-form');
   //$('#container input[placeholder*=Name]').addClass('highlight-form'); // highlights all name fields
-  $('#containers').find('.hot').children('.solid').addClass('highlight-things');
+  $('#container').find('.hot').children('.solid').addClass('highlight-things');
   $('#snow').parent().find('.title').addClass('highlight-things');
   $('.box').on('click','.box-button', function(){
     $('.box').toggleClass('highlight-click');
+  });
+  $('#select-menu').on('change', function() {
+    let name = $('#select-menu option:selected').text();
+    let distance = $('#select-menu option:selected').val();
+    let price = $('#select-menu option:selected').data('price');
+    if (distance) {
+      $('#feedback-message').text('You are signing up for a ' + name + ', which costs ' + price + ', to a distance of ' + distance + 'km');
+    } else {
+      $('#feedback-message').empty();
+    }
+
   });
 });
