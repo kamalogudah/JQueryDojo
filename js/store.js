68e33d16fc455aa0ae9948d8a9c58ad9629be098
kamalogudah <mcpaul2058@gmail.com>
2019-11-12 12:16:17 +0300

    Remove Elements

--- js/store.js
+++ js/store.js
@@ -9,12 +9,16 @@ $().ready(function(){
     html += '<img src="assets/diani-beach-safari.jpg" alt="It is the deal">';
     html += '<div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa velit nobis repellat modi quibusdam, illum expedita voluptate! Sapiente, unde hic enim, expedita doloremque similique numquam at iure optio libero sit!</div>';
     html += '<div class="price">499</div>';
-    html += '<button>Add to Cart</button>';
+    html += '<button class="item-add">Add to Cart</button>';
+    html += '<button class="item-remove">Remove</button>';
     html += '<br>';
     html += '<a href="">More Info</a>';
     html += '<div class="more-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quisquam, iure libero blanditiis architecto repellendus, vero, perspiciatis aspernatur eius eos omnis cum obcaecati mollitia nemo quam neque porro debitis molestiae.</div>';
     html += '</div>';
     $('#container').append(html);
   
+  });
+  $('#container').on('click','.item-remove', function(){
+    $(this).parent().remove();
   })
 })
