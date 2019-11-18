521241681bf18bfcf2117cdb272dd278f1bab727
kamalogudah <mcpaul2058@gmail.com>
2019-11-19 02:03:40 +0300

    Loading Json

--- js/store.js
+++ js/store.js
@@ -1,23 +1,23 @@
 $().ready(function(){
-  $('#button-create-item').on('click', function(){
-    let name = $('#input-create-item').val();
-    $('#input-create-item').val('');
+  function addItem(name, description, price, moreInfo){
+
     let html = '';
 
     html += '<div class="item">';
     html += '<div class="name">' + name + '</div>';
     html += '<img src="assets/diani-beach-safari.jpg" alt="It is the deal">';
-    html += '<div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa velit nobis repellat modi quibusdam, illum expedita voluptate! Sapiente, unde hic enim, expedita doloremque similique numquam at iure optio libero sit!</div>';
-    html += '<div class="price">499</div>';
+    html += '<div class="description">' + description + '</div>';
+    html += '<div class="price">' + price + '</div>';
     html += '<button class="item-add">Add to Cart</button>';
     html += '<button class="item-remove">Remove</button>';
     html += '<br>';
     html += '<a class="more-info-link" href="">More Info</a>';
-    html += '<div class="more-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quisquam, iure libero blanditiis architecto repellendus, vero, perspiciatis aspernatur eius eos omnis cum obcaecati mollitia nemo quam neque porro debitis molestiae.</div>';
+    html += '<div class="more-info">' + moreInfo + '</div>';
     html += '</div>';
-    $('#container').append(html);
+    $('#container').prepend(html);
+
+  }
   
-  });
 
   $('#container').on('click','.more-info-link', function(e){
     e.preventDefault();
@@ -28,8 +28,23 @@ $().ready(function(){
     $(this).parent().remove();
   });
 
-  $.ajax('../data/item.json', function(response){
-    console.log(response);
-  });
+  $.ajax('/data/item.json', { 
+    dataType: 'json',
+    contentType: 'application/json',
+    cache: false
+
+
+  })
+  .done(function(response){
+    let items = response.items;
+    items.forEach(function(item){
+      addItem(item.name, item.description, item.price, item.moreInfo)
+    })
+  }).fail(function(request, errorType, errorMessage){
+    console.log(errorMessage);
+
+  }).always(function(){
+
+  })
 
 });
